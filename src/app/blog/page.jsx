323678be02f00bd65e8df2c9a4a06da6e6e661b3
xtadeushx <alekscherkasov13@gmail.com>
2023-06-14 18:47:59 +0300

    init project

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,7 @@
+import styles from './page.module.css'
+
+export default function Blog() {
+  return (
+    <div className="">Blog</div>
+  )
+}
---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,7 @@
+import styles from './page.module.css'
+
+export default function Dashboard() {
+  return (
+    <div className="">Dashboard</div>
+  )
+}
