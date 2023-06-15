71d4d19fa385aee5f64708c276654bfc3522a804
xtadeushx <alekscherkasov13@gmail.com>
2023-06-15 10:22:48 +0300

    feat: add links for gitHUb and Linkedin

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import styles from "./footer.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>©2023 Lamamia. All rights reserved.</div>
       <div className={styles.social}>
-        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Lama Dev Facebook Account" />
-        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
+        <Link href='https://uk-ua.facebook.com/' target="_blank">
+          <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Lama Dev Facebook Account" />
+        </Link>
+        <Link href='https://www.linkedin.com/feed/' target="_blank">
+          <Image src="/linkedin.png" width={30} height={30} className={styles.icon} alt="xtadeushx Dev  Linkedin" />
+        </Link>
+        <Link href='https://github.com/xtadeushx?tab=repositories' target="_blank">
+          <Image src="/github.png" width={25} height={25} className={styles.icon} alt="xtadeushx Dev  gitHub" />
+        </Link>
+        <Link href='https://www.instagram.com/' target="_blank">
+          <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
+        </Link>
         <Image src="/3.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
-        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
       </div>
     </div>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,40 +1,10 @@
 'use client'
 
-import Link from 'next/link'
-import React from 'react'
-import styles from './navbar.module.css'
-const LINKS = [
-  {
-    id: 1,
-    title: "Home",
-    url: "/",
-  },
-  {
-    id: 2,
-    title: "Portfolio",
-    url: "/portfolio",
-  },
-  {
-    id: 3,
-    title: "Blog",
-    url: "/blog",
-  },
-  {
-    id: 4,
-    title: "About",
-    url: "/about",
-  },
-  {
-    id: 5,
-    title: "Contact",
-    url: "/contact",
-  },
-  {
-    id: 6,
-    title: "Dashboard",
-    url: "/dashboard",
-  },
-];
+import Link from 'next/link';
+import React from 'react';
+import styles from './navbar.module.css';
+import { LINKS } from '@/constants/links';
+
 const Navbar = () => {
   return (
     <div className={styles.container}>
