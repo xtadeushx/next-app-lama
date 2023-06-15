3afe5d083c6f0ac35417861739769a16086cdb7c
xtadeushx <alekscherkasov13@gmail.com>
2023-06-15 16:21:54 +0300

    feat: add contact page with styles Closes #5

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,37 @@
+import Image from 'next/image'
 import styles from './page.module.css'
+import Button from '@/components/button/Button'
 export default function Contact() {
   return (
-    <div className={styles.container}>Contact</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}></h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src='/contact.png'
+            fill={true}
+            alt='contacts'
+            className={styles.image}
+          />
+        </div>
+        <form action="" className={styles.form}>
+          <input
+            className={styles.input}
+            placeholder='name'
+            type="text" />
+          <input
+            className={styles.input}
+            placeholder='email'
+            type="email" />
+          <textarea
+            className={styles.textArea}
+            placeholder='message'
+            rows={10}
+            cols={30}
+          />
+          <Button url='#' text='Send' type='submit' />
+        </form>
+      </div>
+    </div>
   )
 }
--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import styles from './button.module.css';
 import Link from 'next/link';
 
-const Button = ({ text = '', url = '' }) => {
+const Button = ({ text = '', url = '', type = 'text' }) => {
   return (
     <Link href={url}>
-      <button className={styles.container}>{text}</button>
+      <button className={styles.container} type={type}>{text}</button>
     </Link>
   )
 }
