6e20c5de64939a536dbec1aba556c9e25359d8c8
xtadeushx <alekscherkasov13@gmail.com>
2023-06-24 14:36:38 +0300

    implement adding of a new user Closes #21 Closes #17

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,16 +1,47 @@
-'use client'
-import Link from 'next/link'
-import styles from './page.module.css'
+'use client';
+import Link from 'next/link';
+import styles from './page.module.css';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
 export default function Register() {
+  const [error, setError] = useState(false);
+  const router = useRouter()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+      res.status === 201 && router.push("/dashboard/login?success=Account has been created");
+    } catch (err) {
+      setError(err);
+      console.log(err);
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <form action="" className={styles.form}>
-        <input type="username" placeholder='username' className={styles.input} required />
-        <input type="email" placeholder='email' className={styles.input} required />
-        <input type="password" placeholder='password' className={styles.input} required />
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input type="username" placeholder="username" className={styles.input} required />
+        <input type="email" placeholder="email" className={styles.input} required />
+        <input type="password" placeholder="password" className={styles.input} required />
         <button className={styles.button}>Register</button>
       </form>
-      <Link href='/dashboard/login'>Login with an existing account</Link>
+      {error && "Something went wrong"}
+      <Link href="/dashboard/login">Login with an existing account</Link>
     </div>
-  )
+  );
 }
