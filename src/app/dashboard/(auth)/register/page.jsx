11636bbb9e9683e5f19f0871fb52ac6d24dcb812
xtadeushx <alekscherkasov13@gmail.com>
2023-06-24 13:43:48 +0300

    add login page and register page Closes #18 Closes #19 Closes #20

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,6 +1,16 @@
-
+'use client'
+import Link from 'next/link'
+import styles from './page.module.css'
 export default function Register() {
   return (
-    <div className="">Register</div>
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <input type="username" placeholder='username' className={styles.input} required />
+        <input type="email" placeholder='email' className={styles.input} required />
+        <input type="password" placeholder='password' className={styles.input} required />
+        <button className={styles.button}>Register</button>
+      </form>
+      <Link href='/dashboard/login'>Login with an existing account</Link>
+    </div>
   )
 }
