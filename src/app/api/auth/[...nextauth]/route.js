3b8fa85577c6d3931e6420bd781ec599404856a3
xtadeushx <alekscherkasov13@gmail.com>
2023-06-24 14:51:57 +0300

    implement credentials Closes #22

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,11 +1,39 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import connect from '@/utils/db';
+import User from '@/models/User';
+
+import bcrypt from 'bcryptjs';
 
 const handler = NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+        CredentialsProvider({
+            id: 'credentials',
+            name: 'Credentials',
+            async authorize(credentials) {
+                await connect();
+
+                try {
+                    const user = await User.findOne({ email: credentials.email });
+                    if (user) {
+                        const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
+                        if (isPasswordCorrect) {
+                            return user;
+                        } else {
+                            throw new Error('wrong credentials')
+                        }
+                    } else {
+                        throw new Error('User not found');
+                    }
+                } catch (error) {
+                    throw new Error(error);
+                }
+            },
         }),
     ],
 });
